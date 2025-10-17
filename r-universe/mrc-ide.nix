a2e8ac19eb840e93aac8c1fb07a47e5522a1ec86
plietar <1489775+plietar@users.noreply.github.com>
2025-10-17 00:57:03 +0000

    Update r-universe

--- r-universe/mrc-ide.nix
+++ r-universe/mrc-ide.nix
@@ -1,7 +1,7 @@
 # This file is generated from generate-r-universe DO NOT EDIT.
 # Execute the following command to update the file.
 #
-# Rscript generate-r-universe.R /nix/store/wraj1ral39idf5qv01n6wancndim4jm5-source
+# Rscript generate-r-universe.R /nix/store/6q5hgy9mwmrlilvp6jk8j7cq3vp1prmg-source
 
 { self, derive }:
 with self; {
@@ -29,7 +29,7 @@ with self; {
   frogger = derive { name = "frogger"; version = "0.1.5"; buildInputs = [Rcpp beers dplyr eppasm hdf5r magrittr Rcpp tidyr abind docopt devtools knitr leapfrog RcppEigen reshape2 rmarkdown SpectrumUtils testthat withr xml2]; };
   gonovax = derive { name = "gonovax"; version = "0.4.23"; buildInputs = [odin dde furrr progress mcstate testthat knitr rmarkdown]; };
   heartbeatr = derive { name = "heartbeatr"; version = "0.6.0"; buildInputs = [R6 redux processx testthat]; };
-  hintr = derive { name = "hintr"; version = "1.2.14"; buildInputs = [digest docopt dplyr fs geojsonio glue ids jsonlite naomi naomi.options porcelain qs R6 readr readxl rlang rrq specio storr traduire V8 yaml zip callr cli covr duckdb httr jsonvalidate knitr mockery pkgload ps redux rmarkdown ssh testthat tidyselect withr]; };
+  hintr = derive { name = "hintr"; version = "1.2.15"; buildInputs = [digest docopt dplyr fs geojsonio glue ids jsonlite naomi naomi.options porcelain qs R6 readr readxl rlang rrq specio storr traduire V8 yaml zip askpass callr cli covr duckdb httr jsonvalidate knitr mockery pkgload ps redux rmarkdown ssh testthat tidyselect withr]; };
   hipercow = derive { name = "hipercow"; version = "1.1.6"; buildInputs = [audio cli fs ids pkgdepends rlang withr bench callr conan2 cyphr dust furrr future knitr logwatch mockery openssl prettyunits redux rmarkdown rrq testthat]; };
   hipercow_dide = derive { name = "hipercow.dide"; version = "1.1.6"; buildInputs = [cli conan2 crayon fs glue hipercow httr ids jsonlite keyring logwatch openssl pkgdepends rematch rstudioapi rlang xml2 mockery testthat withr]; };
   individual = derive { name = "individual"; version = "0.1.18"; buildInputs = [R6 Rcpp ggplot2 knitr mockery rmarkdown pkgdown testthat xml2 bench Rcpp testthat]; };
